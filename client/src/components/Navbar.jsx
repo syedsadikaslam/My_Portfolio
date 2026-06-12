6fe01edcacd048fd7910fea217c40fc11e015835
Md Sadik <152442843+syedsadikaslam@users.noreply.github.com>
2026-06-12 18:22:11 +0530

    Update LinkedIn link in Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -197,7 +197,7 @@ const Navbar = () => {
           <a href="https://github.com/syedsadikaslam" target="_blank" rel="noreferrer" className="text-secondary hover:text-primary transform hover:scale-110 transition-all">
             <svg width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /></svg>
           </a>
-          <a href="https://www.linkedin.com/in/Md-Sadik-9104a2252" target="_blank" rel="noreferrer" className="text-secondary hover:text-primary transform hover:scale-110 transition-all">
+          <a href="https://www.linkedin.com/in/sadikaslam" target="_blank" rel="noreferrer" className="text-secondary hover:text-primary transform hover:scale-110 transition-all">
             <svg width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect width="4" height="12" x="2" y="9" /><circle cx="4" cy="4" r="2" /></svg>
           </a>
         </div>
